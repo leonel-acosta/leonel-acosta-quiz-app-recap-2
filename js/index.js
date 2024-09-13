98a46d9c3f776e5168f87a3f16f79de37efd1cb8
leonel-acosta <leoacosta88@gmail.com>
2024-09-13 10:40:30 +0200

    Create Dark mode functionality and styling

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.clear()
+
+// Dark mode function
+const bodyElement = document.querySelector('[data-js="body"]')
+const darkModeButton = document.querySelector('[data-js="dark-mode-button"]')
+
+darkModeButton.addEventListener("click", (event) => {
+    bodyElement.classList.toggle("dark");
+
+    if (darkModeButton.innerHTML == "DISABLED") {
+        darkModeButton.innerHTML = "ENABLED";
+        console.log("Dark mode: Enabled")
+        }
+    else if (darkModeButton.innerHTML == "ENABLED"){
+        darkModeButton.innerHTML = "DISABLED"
+        console.log("Dark mode: Disabled")
+    }
+  });
+
