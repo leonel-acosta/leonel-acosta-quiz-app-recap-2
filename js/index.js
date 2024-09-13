d235a61a2871d79e7a8d0dc2bdb602f13262747c
leonel-acosta <leoacosta88@gmail.com>
2024-09-13 12:03:55 +0200

    build button show answer interaction

--- js/index.js
+++ js/index.js
@@ -10,6 +10,19 @@ bookmark.addEventListener("click", (event) => {
     else if (bookmark.src.includes("assets/bookmark_filled.png")){
         bookmark.src = "assets/bookmark.png"
         console.log("Bookmark removed")
-    }
-}
-)
+    };
+})
+
+// Show answer button function
+
+const answerContainer = document.querySelector('[data-js="answer-container"]')
+const showAnswer  = document.querySelector('[data-js="show-answer"]')
+const answer  = document.querySelector('[data-js="answer"]')
+
+answerContainer.addEventListener("click", (event) => {
+    event.preventDefault()
+    answer.removeAttribute("hidden");
+    showAnswer.hidden = true;
+    console.log("Show Answer")
+})
+
