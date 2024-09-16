b00275471400b0d1ef05ede376c5c25555d19eb0
leonel-acosta <leoacosta88@gmail.com>
2024-09-16 10:57:22 +0200

    fix card container

--- js/form.js
+++ js/form.js
@@ -11,16 +11,16 @@ form.addEventListener("submit",(event) => {
     event.preventDefault()
     const customCard = document.createElement("div")
     customCard.classList.add("card__wrapper")
-    customCard.immerHTML = `
+    customCard.innerHTML = `
                 <img class="bookmark" src="assets/bookmark.png" data-js="bookmark">
                 <div class="question__card first__question" data-js="question-card">
-                    <h2 class="question__text" data-js="question-text">${question}</h2>
+                    <h2 class="question__text" data-js="question-text">${question.value}</h2>
                     <div class="answer__container" tabindex="0" data-js="answer-container">
                         <h3 class="show_answer__text" aria-label="show answer" data-js="show-answer">Show answer</h3>
-                        <h3 class="answer__text hidden" data-js="answer" >${answer}</h3>
+                        <h3 class="answer__text hidden" data-js="answer" >${answer.value}</h3>
                     </div>
                     <div class="categories__container" data-js="categories-container">
-                        <h4 class="category__tag" data-js="category-tag">${tags}</h4>
+                        <h4 class="category__tag" data-js="category-tag">${tags.value}</h4>
                     </div>
                 </div>
     `
